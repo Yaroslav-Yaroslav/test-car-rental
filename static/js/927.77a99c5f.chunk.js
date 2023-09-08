8ae38a2e0e13c7ce0fffff1382468b670df7707a
Yaroslav-Yaroslav <111749401+Yaroslav-Yaroslav@users.noreply.github.com>
2023-09-08 18:12:11 +0000

    Deploying to gh-pages from @ Yaroslav-Yaroslav/test-car-rental@c2b2707f067a950d6da1d4a7b75b0aa99046cd9a 🚀

--- static/js/927.77a99c5f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_car_rental=self.webpackChunktest_car_rental||[]).push([[927],{5927:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Favorite cars page"})}}}]);
-//# sourceMappingURL=927.77a99c5f.chunk.js.map
